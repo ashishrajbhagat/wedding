b3a608dfcfba754a7310f3f99df6e7f3e616b877
Ashish Raj <40534661+ashishrajbhagat@users.noreply.github.com>
2024-11-29 14:04:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000);
+}
+
 // Countdown Timer
 const weddingDate = new Date("2025-04-16T00:00:00").getTime();
 
